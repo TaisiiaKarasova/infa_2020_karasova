6609f6c6ddd452c603075df8da5bca5419a78e3c
Taisiia Karasova <karasova.t@phystech.edu>
2020-10-16 10:00:31 +0300

    Corrected compilation error

--- lab6/tasks_1-6.py
+++ lab6/tasks_1-6.py
@@ -272,7 +272,7 @@ while not finished and time_left > 0:
                 points = hitting_the_targets(event, points)
         if finished == True:
             break
-         if time_left <= 0:
+        if time_left <= 0:
             break
         draw_logos()
         print_number_up_right_corner (points)
